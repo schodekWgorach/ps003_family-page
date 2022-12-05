f82294b46071366f0865177ca4f1d3780f5c37c5
schodekWgorach <sschodnicki@gmail.com>
2022-12-05 16:26:47 +0100

    arranging folders v 1

---
+++ src/components/Main/fourCards/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FourCards.scss';
+
+const FourCards = () => {
+    return (
+        <div className="FourCards_box">
+            FourContainer
+        </div>
+    );
+};
+
+export default FourCards;
---
+++ src/components/Main/fourContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FourContainer.scss';
+
+const index = () => {
+    return (
+        <div className="FourContainer_box">
+            FourContainer
+        </div>
+    );
+};
+
+export default index;
